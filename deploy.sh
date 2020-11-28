5a3bf0583a2463c8932f4baa461d7af5322d5298
bclswl0827 <24319042+bclswl0827@users.noreply.github.com>
2020-11-28 21:43:36 +0800

    Deployment script added.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 
+# Define global environment variables
+IMAGE_TAG="registry.cn-shanghai.aliyuncs.com/flydog-sdr/flydog-sdr:latest"
+LOCAL_IMAGE_ID=$(docker inspect -f {{".Id"}} ${IMAGE_TAG})
+
+# Remove previous container and old image
+docker rm -f flydog-sdr
+docker image rm -f ${LOCAL_IMAGE_ID}
+
+# Pull new image and deploy
+docker run -d \
+   --hostname flydog-sdr \
+   --name flydog-sdr \
+   --network flydog-sdr \
+   --privileged \
+   --publish 8073:8073 \
+   --restart always \
+   --volume kiwi.config:/root/kiwi.config \
+   ${IMAGE_TAG}
