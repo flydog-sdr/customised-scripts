9732834ed1c877c51dcf1ce0d2d6b54203ca38ab
bclswl0827 <24319042+bclswl0827@users.noreply.github.com>
2021-01-16 22:53:54 +0800

    Force self-update.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,5 @@
 #!/bin/bash -e
 
-# Update Frpc configuration for old version
-sed -e "s/login_fail_exit = true/login_fail_exit = false/g" \
-    -i /etc/kiwi.config/frpc*
-
 # Define global environment variables
 IMAGE_TAG="registry.cn-shanghai.aliyuncs.com/flydog-sdr/flydog-sdr:latest"
 LOCAL_IMAGE_ID=$(docker inspect -f {{".Id"}} ${IMAGE_TAG})
@@ -22,3 +18,21 @@ docker run -d \
    --restart always \
    --volume kiwi.config:/root/kiwi.config \
    ${IMAGE_TAG}
+
+# Update Frpc configuration for old version
+sed -e "s/login_fail_exit = true/login_fail_exit = false/g" \
+    -i /etc/kiwi.config/frpc*
+
+# Self-update for /usr/bin/updater.sh
+cat /tmp/customised-scripts/self-update.txt > /usr/bin/updater.sh
+
+# Purge previous image for saving disk space
+echo "Purging all unused or dangling images, containers."
+docker container prune -f
+docker image prune -f
+
+# Finish upgrading stage
+rm -rf /tmp/customised-scripts
+echo "Upgrade finished!"
+
+exit 0
