b9198f7e16f681660111d71722b4d3f00b7a464c
bclswl0827 <24319042+bclswl0827@users.noreply.github.com>
2021-01-16 20:16:57 +0800

    Update Frpc configuration for old version.

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -e
 
+# Update Frpc configuration for old version
+sed -e "s/login_fail_exit = true/login_fail_exit = false/g" \
+    -i /etc/kiwi.config/frpc*
+
 # Define global environment variables
 IMAGE_TAG="registry.cn-shanghai.aliyuncs.com/flydog-sdr/flydog-sdr:latest"
 LOCAL_IMAGE_ID=$(docker inspect -f {{".Id"}} ${IMAGE_TAG})
